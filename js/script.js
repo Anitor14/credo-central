d94627c87cab01526ef57a86ad69c9b3ea9b474e
Anitor <anitorabraham@gmail.com>
2022-08-29 22:59:16 +0100

    buidling hoverin modals

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 const sidebar = document.querySelector(".sidebar");
 const closeBtn = document.querySelector(".sidebar-close");
 const navBtn = document.querySelector(".nav-btn");
+const navbar = document.querySelector(".navigation-pane");
+
+// adding fixed class to navbar when
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
 
 //show Sidebar
 navBtn.addEventListener("click", function () {
-  //   sidebar.classList.add("show-sidebar");
-  sidebar.style.display = "flex";
+  console.log("this should show the sidebar");
+  // sidebar.classList.add("show-sidebar");
+    sidebar.style.display = "flex";
 });
 //remove Sidebar
 closeBtn.addEventListener("click", function () {
   console.log("this should close the sidebar");
-  //   sidebar.classList.remove("show-sidebar");
-  sidebar.style.display = "none";
+  // sidebar.classList.remove("show-sidebar");
+    sidebar.style.display = "none";
 });
